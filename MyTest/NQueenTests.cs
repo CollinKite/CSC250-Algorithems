aec89b48ac70f3a8814dd3d6c3fcf3a7f793ba1a
Collin Kite <me@collinkite.com>
2022-04-23 12:21:21 -0600

    queensSetup

--- MyTest/NQueenTests.cs
+++ MyTest/NQueenTests.cs
@@ -1,23 +1,23 @@
-﻿//using System.Collections.Generic;
-//using NUnit.Framework;
-//using N_Queens;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using N_Queens;
 
-//namespace MyTest
-//{
-//    public class NQueenTests
-//    {
-//        [Test]
-//        public void TestIsSafeWithInvalidQueen()
-//        {
-//            bool result = NQueensSolver.IsQueenSafe();
-//            Assert.IsFalse(result);
-//        }
+namespace MyTest
+{
+    public class NQueenTests
+    {
+        [Test]
+        public void TestIsSafeWithInvalidQueen()
+        {
+            bool result = NQueensSolver.IsQueenSafe();
+            Assert.IsFalse(result);
+        }
 
-//        [Test]
-//        public void TestIsSafeWithvalidQueen()
-//        {
-//            bool result = NQueensSolver.IsQueenSafe();
-//            Assert.IsTrue(result);
-//        }
-//    }
-//}
+        [Test]
+        public void TestIsSafeWithvalidQueen()
+        {
+            bool result = NQueensSolver.IsQueenSafe();
+            Assert.IsTrue(result);
+        }
+    }
+}
--- N Queens/NQueensSolver.cs
+++ N Queens/NQueensSolver.cs
@@ -1,41 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace N_Queens
-//{
-//    public static class NQueensSolver
-//    {
-//        //Solution List
-//        private static int Steps = 0;
-//        public static void Go(int n)
-//        {
-//            int[,] Board = new int[n, n];
-//            int[] Queens = new int[n];
-//            SolveNQueens(n);
+namespace N_Queens
+{
+    public static class NQueensSolver
+    {
+        private static int steps = 0;
+        public static void Go(int n)
+        {
+            int[] queens = new int[n];
+            SolveNQueens(queens, n);
+        }
 
-//        }
+        public static void SolveNQueens( int[] queens, int n)
+        {
+            
+        }
 
-//        public static void SolveNQueens(int row, )
-//        {
-//            if (!IsQueenSafe)
-//            {
-
-//                return;
-//            }
-//        }
-
-//        public static bool IsQueenSafe()
-//        { 
-//            return false;
-//        }
-//    }
-
-//    public static class Solution
-//    {
-        
-//    }
-
-//}
+        public static bool IsQueenSafe(int)
+        {
+            return false;
+        }
+    }
+}
